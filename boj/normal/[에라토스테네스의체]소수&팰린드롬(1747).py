5486da348511c62f47a0b76a8436ce5ef1360f56
[3기-2반] 최성호 <c99@kakao.com>
2021-11-09 00:38:20 +0900

    clear

--- boj/normal/[에라토스테네스의체]소수&팰린드롬(1747).py
+++ boj/normal/[에라토스테네스의체]소수&팰린드롬(1747).py
@@ -1,11 +1,22 @@
-n = map(int, input().split())
+n = int(input())
 
-arr = [True for _ in range(n+1)]
-arr[1] = False
 
-for i in range(2, n+1):
-    if arr[i]:
-        for j in range(2*i, n+1, i):
-            arr[j] = 0
-        if str(i) == ''.join(list(str(i))[::-1]):
-            print(i)
+def solution(n):
+    max = 1_000_000
+
+    arr = [True for _ in range(max+1)]
+    arr[1] = False
+
+    for i in range(2, max+1):
+        if arr[i]:
+            if i >= n:
+                if str(i) == str(i)[::-1]:
+                    return i
+            for j in range(2*i, max+1, i):
+                arr[j] = False
+
+    return 1003001
+
+
+result = solution(n)
+print(result)
